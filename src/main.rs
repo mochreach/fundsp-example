7ee0dbc2ee5850ba9a888155ac294f2d8ab9b7e7
Chris Wells Wood <cwwoodesq@gmail.com>
2024-01-02 16:20:30 +0000

    Fixes comment on FM synthesiser.

--- src/main.rs
+++ src/main.rs
@@ -113,10 +113,10 @@ fn create_sine_440() -> Box<dyn AudioUnit64> {
 /// C major chord created by summing waves! Sine by default, but try uncommenting
 /// the other wave types.
 fn create_c_major() -> Box<dyn AudioUnit64> {
-    let synth = sine_hz(261.6) + sine_hz(329.628) + sine_hz(391.995);
+    // let synth = sine_hz(261.6) + sine_hz(329.628) + sine_hz(391.995);
     // let synth = square_hz(261.6) + square_hz(329.628) + square_hz(391.995);
     // let synth = soft_saw_hz(261.6) + soft_saw_hz(329.628) + soft_saw_hz(391.995);
-    // let synth = hammond_hz(261.6) + hammond_hz(329.628) + hammond_hz(391.995);
+    let synth = hammond_hz(261.6) + hammond_hz(329.628) + hammond_hz(391.995);
 
     Box::new(synth)
 }
@@ -138,12 +138,12 @@ fn create_sample_with_reverb() -> Box<dyn AudioUnit64> {
         Arc::new(Wave64::load("samples/closed_high_hat.wav").expect("Could not find sample file."));
     let left = wave64(&wave, 0, None);
     let right = wave64(&wave, 1, None);
-    let synth = (left | right) >> (multipass() & (1.0 * reverb_stereo(10.0, 3.0)));
+    let synth = (left | right) >> (multipass() & (0.2 * reverb_stereo(10.0, 3.0)));
 
     Box::new(synth)
 }
 
-// Simple sine wave at 440 Hz which is standard tuning for A4
+// Simple FM synthesiser taken from the FunDSP docs
 fn create_simple_fm() -> Box<dyn AudioUnit64> {
     // Frequency
     let f = 440.0;
